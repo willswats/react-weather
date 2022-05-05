0b4a06b6c66718cf1e77cb054b74972caf43530a
willswats <williamstuwatson@gmail.com>
2022-05-05 23:45:20 +0100

    Remove search suggestions

--- src/components/Weather/WeatherSearch.js
+++ src/components/Weather/WeatherSearch.js
@@ -1,15 +1,13 @@
 import { ACTIONS, MEASUREMENTS } from '../../App';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-import Search from '../UI/Search/Search';
-import List from '../UI/Search/List';
+import Search from '../UI/Search';
 
 import { fetchGeocodingData } from '../../helpers/api';
 
 const WeatherSearch = ({ measurement, dispatch, setWeatherData }) => {
   const [search, setSearch] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -35,30 +33,12 @@ const WeatherSearch = ({ measurement, dispatch, setWeatherData }) => {
         payload: { message: `"${search}" is not a city.` },
       });
     }
-    // Reset state
     setSearch('');
-    setSuggestions([]);
   };
 
   const changeHandler = (event) => {
     setSearch(event.target.value);
   };
-
-  useEffect(() => {
-    const timer = setTimeout(async () => {
-      if (search.length > 0) {
-        let dataArr = [];
-        const data = await fetchGeocodingData(search);
-        for (let i = 0; i < data.length; i++) {
-          dataArr.push({ name: data[i].name, country: data[i].country });
-        }
-        setSuggestions(dataArr);
-      } else {
-        setSuggestions([]);
-      }
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [search]);
 
   return (
     <>
@@ -67,7 +47,6 @@ const WeatherSearch = ({ measurement, dispatch, setWeatherData }) => {
         changeHandler={changeHandler}
         value={search}
         placeholder={'Search City'}
-        list={<List items={suggestions} />}
       />
     </>
   );
