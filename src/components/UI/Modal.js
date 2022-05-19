02292a8d372ba8d5b92857cd37f906243e2a26eb
willswats <williamstuwatson@gmail.com>
2022-05-19 16:18:34 +0100

    Remove modal cross

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,13 +1,8 @@
 import classes from './Modal.module.css';
 
-import { ReactComponent as IconCross } from '../../svgs/close-line.svg';
-
 const Overlay = ({ hideHandler, body }) => {
   return (
     <>
-      <div className={classes['cross']} onClick={hideHandler}>
-        <IconCross />
-      </div>
       <div onClick={hideHandler} className={classes['overlay']} />
       <div className={classes['body']}>{body}</div>
     </>
