1cee9cb22e1fd31d29edcf45863fb8fa6ed9ec5a
willswats <williamstuwatson@gmail.com>
2022-05-19 16:14:01 +0100

    Add cross to Modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,8 +1,13 @@
 import classes from './Modal.module.css';
 
+import { ReactComponent as IconCross } from '../../svgs/close-line.svg';
+
 const Overlay = ({ hideHandler, body }) => {
   return (
     <>
+      <div className={classes['cross']} onClick={hideHandler}>
+        <IconCross />
+      </div>
       <div onClick={hideHandler} className={classes['overlay']} />
       <div className={classes['body']}>{body}</div>
     </>
