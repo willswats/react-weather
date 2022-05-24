2c68298caaf774b35bc70b7ae0656d3f95516bfe
willswats <williamstuwatson@gmail.com>
2022-05-24 10:21:44 +0100

    Add search icon

--- src/components/UI/Search.js
+++ src/components/UI/Search.js
@@ -2,7 +2,7 @@ import classes from './Search.module.css';
 
 const Search = ({ submitHandler, changeHandler, value, placeholder }) => {
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes['form']} onSubmit={submitHandler}>
       <input
         type="text"
         onChange={changeHandler}
