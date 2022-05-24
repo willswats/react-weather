bddb7ac9ef3c17ee9f147327458bb97ff5c42a67
willswats <williamstuwatson@gmail.com>
2022-05-24 10:44:57 +0100

    Update search icon styles

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -18,7 +18,7 @@ export const fetchReverseGeocodingData = async (lat, lon) => {
 
 export const fetchGeocodingData = async (city) => {
   const response = await fetch(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
   );
   const data = await response.json();
 
